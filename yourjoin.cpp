badb6d742a0e9e34f4c78ffe25da9889098080dd
EP2024 12302386 <ep12302386@g0.complang.tuwien.ac.at>
2025-01-13 17:45:27 +0100

    Read file in parallel

--- yourjoin.cpp
+++ yourjoin.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <algorithm>
 
+#include <thread>
+#include <mutex>
+#include <future>
+
 // Function to split a string by a delimiter
 std::vector<std::string> split(const std::string& line, char delimiter) {
     std::vector<std::string> fields;
@@ -42,12 +46,31 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+
+    //std::cout << "load files\n";
+
     // Step 1: Load the files into multimaps
     // Each multimap allows multiple values for the same key
-    auto file1 = loadFile(argv[1], 0); // Key: First field of File 1, Value: Second field
-    auto file2 = loadFile(argv[2], 0); // Key: First field of File 2, Value: Second field
-    auto file3 = loadFile(argv[3], 0); // Key: First field of File 3, Value: Second field
-    auto file4 = loadFile(argv[4], 0); // Key: First field of File 4, Value: Second field
+    //auto file1 = loadFile(argv[1], 0); // Key: First field of File 1, Value: Second field
+    //auto file2 = loadFile(argv[2], 0); // Key: First field of File 2, Value: Second field
+    //auto file3 = loadFile(argv[3], 0); // Key: First field of File 3, Value: Second field
+    //auto file4 = loadFile(argv[4], 0); // Key: First field of File 4, Value: Second field
+
+    // Declare multimaps for storing file data
+    std::unordered_multimap<std::string, std::string> file1, file2, file3, file4;
+
+    // Use std::async to load files in parallel
+    auto future1 = std::async(std::launch::async, [&]() { return loadFile(argv[1], 0); });
+    auto future2 = std::async(std::launch::async, [&]() { return loadFile(argv[2], 0); });
+    auto future3 = std::async(std::launch::async, [&]() { return loadFile(argv[3], 0); });
+    auto future4 = std::async(std::launch::async, [&]() { return loadFile(argv[4], 0); });
+
+    // Retrieve the results
+    file1 = future1.get();
+    file2 = future2.get();
+    file3 = future3.get();
+    file4 = future4.get();
+
 
     // Step 2: Perform the join operation
     // We need to join the first three files on their first fields
@@ -61,6 +84,9 @@ int main(int argc, char* argv[]) {
     // 5. Second field of file4
 
     // Step 2.1: Iterate over file1 to start the join process
+
+    //std::cout << "start\n";
+
     for (const auto& [key1, value1] : file1) {
         // key1: First field of file1
         // value1: Second field of file1
@@ -95,6 +121,7 @@ int main(int argc, char* argv[]) {
             }
         }
     }
+    //std::cout << "end\n";
 
     // Step 4: Completion
     // The program outputs all matching records in the required format
